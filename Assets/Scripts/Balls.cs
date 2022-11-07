67e22075a5e2dc3ef29b22aa4368fc6855ecbeb0
Rudy Escobar <67067784+rescobar9@users.noreply.github.com>
2022-11-06 21:29:12 -0800

    Changes
    Changed the code to move the balls

--- Assets/Scripts/Balls.cs
+++ Assets/Scripts/Balls.cs
@@ -2,17 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Collections.Specialized;
 
 public class Balls : MonoBehaviour
 {
     public Vector3 startForce;
+    [SerializeField] private float m_MovePower = 5;
+    [SerializeField] private bool m_UseTorque = true;
+    [SerializeField] private float m_MaxAngularVelocity = 25;
+    [SerializeField] private float m_JumpPower = 2;
+
+    private const float k_GroundRayLength = 1f;
+    private Rigidbody m_Rigidbody;
     
 
     // Start is called before the first frame update
     void Start()
     {
+        m_Rigidbody = GetComponent<Rigidbody>();
         Rigidbody rigidbody = GetComponent<Rigidbody>();
         rigidbody.AddForce(startForce, ForceMode.Impulse);
+        GetComponent<Rigidbody>().maxAngularVelocity = m_MaxAngularVelocity;
     }
 
     // Update is called once per frame
@@ -21,20 +31,22 @@ public class Balls : MonoBehaviour
         
     }
 
-    public float speed
+    public void Move(Vector3 moveDirection, bool jump)
     {
-        get { return Time.deltaTime; }
-        set
+        if (m_UseTorque)
         {
-            if (value < 0.0f) 
-            { 
-             value = 5.0f; 
-            }
-            else 
-            { 
-             value = -value; 
-            }
+            m_Rigidbody.AddTorque(new Vector3(moveDirection.z, 0, -moveDirection.x) * m_MovePower);
+        }
+        else
+        {
+            m_Rigidbody.AddForce(moveDirection * m_MovePower);
+        }
+
+        if (Physics.Raycast(transform.position, -Vector3.up, k_GroundRayLength) && jump)
+        {
+            m_Rigidbody.AddForce(Vector3.up * m_JumpPower, ForceMode.Impulse);
         }
     }
 
+    
 }
